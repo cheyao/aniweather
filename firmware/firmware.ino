5962962849ae1c0ab6235e6b70d71eca1f929c58
cheyao <94928179+cheyao@users.noreply.github.com>
2024-12-10 22:18:44 +0100

    Start of firmware

---
+++ firmware/firmware.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_ST7735.h>
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+
+#define TFT_CS 3
+#define TFT_RESET 2
+
+void setup() {
+	Serial.begin(9600);
+	Serial.print("Aniweather booting up...");
+}
+
+void loop() {
+}
